4968eeb7832ac6883cfb230d78139082cb6abc2b
kelly <1532527410@qq.com>
2024-05-26 20:31:33 +0800

    修改basicModal

--- src/BasicModal/demo/Demo.tsx
+++ src/BasicModal/demo/Demo.tsx
@@ -3,22 +3,27 @@ import { BasicModal } from 'KellyCOM';
 import React, { useState } from 'react';
 
 const Demo = () => {
-  const [visible, setVisible] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <Button
         type="primary"
         onClick={() => {
-          setVisible(true);
+          setOpen(true);
         }}
       >
         打开可拖拽弹出框
       </Button>
       <BasicModal
+        open={open}
         width="50%"
         title="可拖拽弹出框"
-        visible={visible}
-        onCancel={() => setVisible(false)}
+        onCancel={handleCancel}
       >
         <div>此弹出框头部可自由拖拽</div>
       </BasicModal>
